274d1dafdd1f941205057a7963946d27e555462f
Younès Rabii <rabiiyounes@outlook.com>
2020-08-27 12:22:22 +0200

    2020 update
    - 2020 update
    - added 2005-2007 small pictures
    - factored some code
    - more comments

--- js/script.js
+++ js/script.js
@@ -2,121 +2,145 @@ function qs(s){return document.getElementById(s)};
 function dc(s){return document.createElement(s)};
 function rand(min,max){return Math.floor(Math.random()*(max-min+1))+min};
 
-var tableIndex   = [53,58,78,63,79,89,112,149,162,165,250,261];
+var tableIndex   = [23,30,52,53,58,78,63,79,89,112,149,162,165,250,261,270];
 var lastInfo;
+var startingYear = 2005;
 
-var 
-fam = qs("fam"),
-load = qs("load"),
-link = qs("link"),
-trans = qs("trans");
+var
+fam = qs("fam"),	 			//case image
+load = qs("load"), 		 //loading gif
+link = qs("source_link"), 		//link to original
+trans = qs("translation_link"); //link to translation
 
-fam.onload = 
+fam.onload =
 function()
 {
 	loader(true);
 };
 
+
+// Generate URLS to image file and official html page
 function genUrl()
 {
 	var yearInput = parseInt(qs("year").value);
-		
+
 	var yearIndex;
-	if(yearInput < 0)
+
+	 // Get a random year, or a fixed one;
+	if(yearInput == -1)
 	{
-		yearIndex = rand(0,tableIndex.length-1); // Get a random year, or a fixed one;
+		yearIndex = rand(0,tableIndex.length-1);
+	}
+	else if(yearInput == -2)
+	{
+		// Avoiding first 3 years small images
+		yearIndex = rand(3,tableIndex.length-1);
 	}
 	else
 	{
+		//yearIndex = 0 to ...14
+		//yearUrl = 05 to ...20
 		yearIndex = yearInput;
 	}
-	
+
 	var imgIndex = rand(1,tableIndex[yearIndex]);
-	var yearUrl  = (yearIndex+8).toString();
-	
-	if(yearUrl.length < 2)
-	{
-		yearUrl = "0"+yearUrl;
-	}
-	
+	var yearUrl  = (yearIndex+startingYear-2000).toString();
+
+	yearUrl = minimumDigits(yearUrl,2);
+
 	var imgUrl, pageUrl;
-	
-	var year = yearIndex+2008;
-	
-	// Calculating the image and page url, see note on bottom
-	if(year < 2017)
-	{
-		imgUrl  = imgIndex.toString();
-		pageUrl = imgIndex.toString(); 
+
+	var year = yearIndex+startingYear;
+
+// Calculating the image and page url, see note on bottom
+
+//Creating hotlinks for images >= 2008
+if(year >= 2008)
+{
+		if(year < 2017)
+		{
+			imgUrl  = imgIndex.toString();
+			pageUrl = imgIndex.toString();
+		}
+		else if(year == 2017)
+		{
+			imgUrl = imgIndex.toString();
+			imgUrl = minimumDigits(imgUrl, 3);
+
+			pageUrl = imgIndex.toString();
+			pageUrl = minimumDigits(pageUrl, 3);
+
+		}
+		else if(year > 2017 && year < 2019)
+		{
+			imgUrl = imgIndex.toString();
+			imgUrl = minimumDigits(imgUrl, 2);
+			imgUrl += "_sample";
+
+			pageUrl = imgIndex.toString();
+			pageUrl = minimumDigits(pageUrl, 3);
+
+		}
+		else
+		{
+			imgUrl = imgIndex.toString();
+			imgUrl = minimumDigits(imgUrl, 3);
+			imgUrl += "_sample";
+
+			pageUrl = imgIndex.toString();
+			pageUrl = minimumDigits(pageUrl, 3);
+
+		}
+
+		return {
+					imgUrl :"http://famicase.com/"+yearUrl+"/softs/"+imgUrl+".jpg",
+					pageUrl:"http://famicase.com/"+yearUrl+"/softs/"+pageUrl+".html",
+					year:year,
+					index:imgIndex
+				};
 	}
-	else if(year == 2017)
+	else // local link for images from 2005 to 2007
 	{
-		imgUrl = imgIndex.toString();
-		while(imgUrl.length < 3)
-		{
-			imgUrl = "0"+imgUrl;
-		}
-		
-		pageUrl = imgIndex.toString();
-		while(pageUrl.length < 3)
-		{
-			pageUrl = "0"+pageUrl;
-		}
-		
+
+			imgUrl = imgIndex.toString();
+			imgUrl = "("+imgUrl+")";
+
+			return {
+						imgUrl :"./img/"+year+"/"+imgUrl+".png",
+						pageUrl:null,
+						year:year,
+						index:imgIndex
+					};
+
 	}
-	else if(year > 2017 && year < 2019)
-	{
-		imgUrl = imgIndex.toString();
-		while(imgUrl.length < 2)
-		{
-			imgUrl = "0"+imgUrl;
-		}
-		imgUrl += "_sample";
-		
-		pageUrl = imgIndex.toString();
-		while(pageUrl.length < 3)
-		{
-			pageUrl = "0"+pageUrl;
-		}
-		
-		
-	}
-	else
-	{
-		imgUrl = imgIndex.toString();
-		while(imgUrl.length < 3)
-		{
-			imgUrl = "0"+imgUrl;
-		}
-		imgUrl += "_sample";
-		
-		pageUrl = imgIndex.toString();
-		while(pageUrl.length < 3)
-		{
-			pageUrl = "0"+pageUrl;
-		}
-		
-	}
-	
-	return {
-				imgUrl :"http://famicase.com/"+yearUrl+"/softs/"+imgUrl+".jpg",
-				pageUrl:"http://famicase.com/"+yearUrl+"/softs/"+pageUrl+".html",
-				year:year,
-				index:imgIndex
-			};
 }
 
+// Change visible links URLS
 function change(urls)
 {
 	loader(false);
+
 	fam.src = urls.imgUrl;
-	link.href = urls.pageUrl;
-	trans.href = "http://www.microsofttranslator.com/bv.aspx?from=&to=en&a="+encodeURI(urls.pageUrl);
-	
+
+	if(urls.pageUrl === null)
+	{ // No official page URL (for 2005 to 2007)
+
+		link.href = "";
+		trans.href = "";
+		enableLinks(false);
+	}
+	else
+	{
+		link.href = urls.pageUrl;
+		trans.href = "http://www.microsofttranslator.com/bv.aspx?from=&to=en&a="+encodeURI(urls.pageUrl);
+		enableLinks(true)
+	}
+
 	lastInfo = urls;
-	
+
 }
 
+// Show or hide loader gif
 function loader(show)
 {
 	if(!show)
@@ -131,21 +155,48 @@ function loader(show)
 	}
 }
 
-
-
-function randomize(data)
+function enableLinks(enable)
 {
-	
-	var rand = genUrl();
-	change(rand);
+	if(enable)
+	{
+		link.className = "";
+		trans.className = "";
+	}
+	else
+	{
+		link.className = "disabledLink";
+		trans.className = "disabledLink";
+	}
 }
 
+
+// Randomize button event
+function randomize(data)
+{
+	var randURL = genUrl();
+	change(randURL);
+}
+
+//
+function minimumDigits(_value, digits)
+{
+	value= _value+""
+
+	while(value.length < digits)
+	{
+		value = "0"+value;
+	}
+	return value
+}
+
+
+
+// Save list manipulation functions
 function save()
 {
 	var list = getList();
-	
-	//Check if not already in list
-	
+
+	//Check if saved item is not already in list
 	for(var i in list)
 	{
 		if(list[i].imgUrl == lastInfo.imgUrl)
@@ -153,17 +204,17 @@ function save()
 			return false;
 		}
 	}
-	
+
 	list.unshift(lastInfo);
 	setList(list);
-	
+
 	refreshList();
 }
 
 function removeFromList(id)
 {
 	var list = getList();
-	
+
 	list.splice(id,1);
 	setList(list);
 	refreshList();
@@ -172,12 +223,12 @@ function removeFromList(id)
 function getList()
 {
 	var list = Lockr.get('saved-list');
-	
+
 	if(typeof list != "object")
 	{
 		return [];
 	}
-	
+
 	return list;
 }
 
@@ -189,15 +240,15 @@ function setList(save)
 function refreshList()
 {
 	var list = getList();
-	
+
 	var parent = qs("list");
-	
+
 	parent.innerHTML = "";
 	list.map(function(element, id)
 	{
 		var li = dc("li");
-		
-		
+
+
 		var label = dc("span");
 		label.innerHTML = `#${element.index} (${element.year})`;
 		label.className = "list-element";
@@ -206,7 +257,7 @@ function refreshList()
 		{
 			change(getList()[id]);
 		}
-		
+
 		 var cross = dc("span");
 		 cross.innerHTML = " ❌"
 		 // cross.dataId = id;
@@ -215,12 +266,12 @@ function refreshList()
 		 {
 			removeFromList(id);
 		 }
-		 
+
 		 parent.appendChild(li);
 		 li.appendChild(label);
 		 li.appendChild(cross);
 	});
-	
+
 }
 
 randomize();
@@ -228,17 +279,18 @@ refreshList();
 
 /*
 	RULES :
-	
+
 	Years always have 2 digits : 08, 09, 10, 11 ...
-	
+
 	Index of images are calculated diffrently for each year :
-	- 2008 to 2016 	: 1		 	 2 		   3		 12		   	123 
+	- 2005 to 2008: stored by art name
+	- 2008 to 2016 	: 1		 	 2 		   3		 12		   	123
 	- 2017			: 001		 002	   003		 012       	123
 	- 2018			: 01_sample  02_sample 03_sample 12_sample 	123_sample
 	- 2019			: 01_sample  02_sample 03_sample 012_sample 123_sample
-	
+
 	Index of html pages too :
 	- 2008 to 2016 	: 01		 02 	   03		 12		   123
 	- 2017 to 2019	: 001		 002	   003		 012       123
-	
+
 */
